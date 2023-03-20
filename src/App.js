c9333bfd02ef8b09257ea85b26c38fadf0261379
Hoang Ngoc Tung <hoangtungpt2001@gmail.com>
2023-03-20 14:48:50 +0700

    init code

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Pagenotfound from "./pages/Pagenotfound";
+import Menu from "./pages/Menu";
+
+
+function App() {
+  return (
+    <div >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element = {<Home />}/>
+          <Route path="/about" element = {<About />}/>
+          <Route path="/contact" element = {<Contact />}/>
+          <Route path="/menu" element = {<Menu />}/>
+          <Route path="*" element = {<Pagenotfound />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
